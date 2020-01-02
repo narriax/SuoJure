78c53ecc84a5e582da88cbc04a8b3ac9e5a882e5
narriax <narriax@gmail.com>
2020-01-02 12:58:57 -0500

    bootstrap medoo

--- public_html/config.php
+++ public_html/config.php
@@ -1,16 +1,45 @@
 <?php
 
+// define main db
+$db_info = array(
+	'main' => array (
+		'hostname' => 'localhost',
+		'dbtype' => 'mysql',
+		'dbname' => 'suojure',
+		'username' => 'root',
+		'password' => '',		
+	),
+);
+
+
+// list required libraries
 global $required;
 $required = array();
 $required['medoo'] = '\_libraries\medoo\src\Medoo.php';
 
 
 
-// check requirements
+// =======================================================
+// TODO: move this to install script?
+
+// include required libraries
 foreach ($required as $r => $rloc) {
 	if (!file_exists(getcwd().$rloc))
 		die ("<div class=error>Required library missing: <b>".$r."</b>: [".$rloc."]</div>");
+	else 
+		include_once(getcwd().$rloc);
 }
 
+// initialize db
+use Medoo\Medoo;
+global $db;
+$db = new Medoo([
+	// required
+	'server' => $db_info['main']['hostname'],
+	'database_type' => $db_info['main']['dbtype'],
+	'database_name' => $db_info['main']['dbname'],
+	'username' => $db_info['main']['username'],
+	'password' => $db_info['main']['password'],
+]);
 
 ?>
