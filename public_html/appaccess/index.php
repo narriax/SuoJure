7e61d300a334e50b0744efc55d10994928030faa
narriax <narriax@gmail.com>
2020-01-03 16:39:40 -0500

    app login and logout

--- public_html/appaccess/index.php
+++ public_html/appaccess/index.php
@@ -5,11 +5,11 @@ if (array_key_exists('action', $_POST)) {
 	
 	switch ($_POST['action']) {
 		case 'user_login':
-			echo app_login('testapp', $_POST['username'], $_POST['password']);
+			echo app_login('testapp', 'testdevice', $_POST['username'], $_POST['password']);
 			break;
 		case 'user_logout':
-			echo app_logout('testapp', $_POST['username'], $_POST['sessionid']);
-			break;		
+			echo app_logout('testapp', $_POST['sessionid']);
+			break;
 		default:
 			echo 'error: unknown request';
 			break;
@@ -18,22 +18,22 @@ if (array_key_exists('action', $_POST)) {
 
 
 
-function app_login($app, $username, $password) {
-
-
-	$code = auth_code($username, $password);
+function app_login($app, $device, $username, $password) {	
+	// app is not currently used
 	
+	$code = auth_code($username, $password);
 	$sessid = '';
 	if ($code == 0) {
-		$sessid = session_open($_POST["username"], $app);		// using app as device, thoughts?
+		$sessid = session_open($_POST["username"], $device);
 	}
-	
 	return $code.':'.$sessid;
 }
 
 
-function app_logout ($app, $username, $password) {
+function app_logout ($app, $sessionid) {
+	// app is not currently used
 	
+	if (!empty($sessionid)) session_close($sessionid);
 }
 
 
