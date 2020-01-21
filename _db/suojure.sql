e365c5f639f7d4444c2e33ea0b7ecd3390bb6b08
Narriax <narriax@gmail.com>
2020-01-21 00:48:54 -0500

    db dump

--- _db/suojure.sql
+++ _db/suojure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 03, 2020 at 11:42 PM
+-- Generation Time: Jan 21, 2020 at 05:45 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.14
 
@@ -21,6 +21,91 @@ SET time_zone = "+00:00";
 --
 -- Database: `suojure`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `taskid` varchar(16) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `owner_uoid` int(16) NOT NULL,
+  `date_archived` timestamp NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_assignments`
+--
+
+DROP TABLE IF EXISTS `task_assignments`;
+CREATE TABLE IF NOT EXISTS `task_assignments` (
+  `uoid` int(16) NOT NULL,
+  `taskid` int(16) NOT NULL,
+  `masterid` int(16) NOT NULL,
+  `status` varchar(16) NOT NULL,
+  `duedate` timestamp NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_history`
+--
+
+DROP TABLE IF EXISTS `task_history`;
+CREATE TABLE IF NOT EXISTS `task_history` (
+  `taskid` int(16) NOT NULL,
+  `userid` int(16) NOT NULL,
+  `newstatus` varchar(16) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_projects`
+--
+
+DROP TABLE IF EXISTS `task_projects`;
+CREATE TABLE IF NOT EXISTS `task_projects` (
+  `uoid` int(16) NOT NULL,
+  `status` varchar(16) NOT NULL,
+  `date_archived` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_status`
+--
+
+DROP TABLE IF EXISTS `task_status`;
+CREATE TABLE IF NOT EXISTS `task_status` (
+  `name` varchar(16) NOT NULL,
+  `allow_for_projects` tinyint(1) NOT NULL,
+  `allow_for_tasks` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `task_status`
+--
+
+INSERT INTO `task_status` (`name`, `allow_for_projects`, `allow_for_tasks`) VALUES
+('new', 1, 1),
+('claimed', 0, 1),
+('started', 1, 1),
+('blocked', 1, 1),
+('paused', 1, 1),
+('finished', 0, 1),
+('accepted', 0, 1),
+('rejected', 0, 1),
+('complete', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -46,16 +131,21 @@ CREATE TABLE IF NOT EXISTS `uobj_data` (
 
 DROP TABLE IF EXISTS `uobj_types`;
 CREATE TABLE IF NOT EXISTS `uobj_types` (
-  `tablename` varchar(16) NOT NULL
+  `tablename` varchar(16) NOT NULL,
+  `uotype_group` varchar(16) NOT NULL,
+  PRIMARY KEY (`tablename`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `uobj_types`
 --
 
-INSERT INTO `uobj_types` (`tablename`) VALUES
-('users'),
-('user_personas');
+INSERT INTO `uobj_types` (`tablename`, `uotype_group`) VALUES
+('users', 'users'),
+('user_personas', 'users'),
+('tasks', 'tasks'),
+('task_assignments', 'tasks'),
+('task_projects', 'tasks');
 
 -- --------------------------------------------------------
 
@@ -82,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`uoid`, `username`, `email`, `password`, `secret`, `date_created`, `date_visited`) VALUES
-(111, 'narriax', 'narriax@gmail.com', 'lord', '', '2020-01-02 20:51:09', '2020-01-04 02:38:46');
+(111, 'narriax', 'narriax@gmail.com', 'lord', '', '2020-01-02 20:51:09', '2020-01-21 10:07:00');
 
 -- --------------------------------------------------------
 
@@ -110,10 +200,18 @@ CREATE TABLE IF NOT EXISTS `user_personas` (
   `uoid` int(16) NOT NULL COMMENT 'unique object id',
   `username` varchar(16) NOT NULL,
   `personaname` varchar(16) NOT NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE KEY `uoid` (`uoid`),
+  PRIMARY KEY (`uoid`) USING BTREE,
+  KEY `username` (`username`),
   KEY `personaname` (`personaname`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_personas`
+--
+
+INSERT INTO `user_personas` (`uoid`, `username`, `personaname`) VALUES
+(2085568596, 'narriax', 'Anri'),
+(1034582707, 'narriax', 'Krey');
 
 -- --------------------------------------------------------
 
@@ -136,10 +234,7 @@ CREATE TABLE IF NOT EXISTS `user_sessions` (
 --
 
 INSERT INTO `user_sessions` (`id`, `username`, `device`, `started`) VALUES
-('1yPS22KuCloWzEphXyE56LAg5cBJKbqv', 'narriax', 'testapp', '2020-01-02 23:14:23'),
-('OZ5JAKngRHmohiWhazkSg41j6E2w9LvS', 'narriax', 'browser', '2020-01-02 22:37:00'),
-('FJV5NWBkMPxAMC3KiGDutCNA7bmKlRKk', 'narriax', 'testapp', '2020-01-02 23:17:11'),
-('s5KM2YUgNXIe8gyiW6r6lMoC21cEIk7t', 'narriax', 'testdevice', '2020-01-03 21:38:46');
+('hiuon2gjpkkflVBZueDbitzqQ6hzIvB2', 'narriax', 'browser', '2020-01-21 05:07:00');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
